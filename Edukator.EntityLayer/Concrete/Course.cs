ae2f3657146a55353056af52c5679c5d6c407bc0
sincomyazilim <sincomyazilim@hotmail.com>
2023-04-18 13:51:46 +0300

    Add project files.

---
+++ Edukator.EntityLayer/Concrete/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrete
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Review { get; set; }//yıldız degerlendırme
+    }
+}
